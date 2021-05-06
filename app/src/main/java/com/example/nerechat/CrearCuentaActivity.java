7acd55cf528433169402118bb8315d8895f5a0b5
nereamonas <nmonasterio003@ikasle.ehu.eus>
2021-05-06 16:47:10 +0200

    Autenticación con Firebase

---
+++ app/src/main/java/com/example/nerechat/CrearCuentaActivity.java
@@ -0,0 +1,110 @@
+package com.example.nerechat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.nerechat.base.BaseActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CrearCuentaActivity extends AppCompatActivity {
+    //Ventana para crear una nueva cuenta
+
+    private TextInputLayout inputemail, inputpass, inputrepPass;
+    FirebaseAuth mAuth;
+    ProgressDialog progressDialog;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_cuenta);
+
+        //Inicializamos las variables
+        inputemail=findViewById(R.id.inputLCrearEmail);
+        inputpass=findViewById(R.id.inputLCrearPass);
+        inputrepPass=findViewById(R.id.inputLCrearRepPass);
+
+        mAuth=FirebaseAuth.getInstance();
+        progressDialog=new ProgressDialog(this);
+
+        TextView text_iniciarSesion= findViewById(R.id.text_iniciarSesion);
+        text_iniciarSesion.setOnClickListener(new View.OnClickListener() {  //Cuando clickemos en el boton camara. pediremos permisos para abrir la camara y guardar las fotos
+            @Override
+            public void onClick(View v) {
+                abrirIniciarSesion();
+            }
+        });
+
+
+
+    }
+
+    public void abrirIniciarSesion(){ //Abrimos la pantalla de inicio de sesion
+        Intent i = new Intent(this, IniciarSesionActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(i);
+        finish();
+    }
+
+    public void abrirCrearPerfil(){ //Abrimos la pantalla de crear un perfil
+        Intent i = new Intent(this, CrearPerfilActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(i);
+        finish();
+    }
+
+    public void registrar(View v){
+        //Click en registrar. comprobamos los datos introducidos, y si son correctos registramos el usuario
+        
+        String email=inputemail.getEditText().getText().toString();
+        String pass=inputpass.getEditText().getText().toString();
+        String repPass=inputrepPass.getEditText().getText().toString();
+
+        if (email.equals("")||!email.contains("@")) {//Para que el email sea valido tiene q ser !="" y contener un @. Si no es asi saltamos un error
+            inputemail.setError("El email no es correcto");
+            inputemail.requestFocus();
+        }else if(pass.equals("")||pass.length()<6){ //La contraseña tiene que tener un minimo de 6 caracteres (Es una restriccion de las pass de firebase). Si no se cumple mostramos mensaje de error
+            inputpass.setError("La contraseña tiene que tener un minimo de 5 caracteres");
+            inputpass.requestFocus();
+        }else if(repPass.equals("")||repPass.length()<6){ //La contraseña tiene que tener un minimo de 6 caracteres (Es una restriccion de las pass de firebase). Si no se cumple mostramos mensaje de error
+            inputrepPass.setError("La contraseña tiene que tener un minimo de 5 caracteres");
+            inputrepPass.requestFocus();
+        }else if (!pass.equals(repPass)) { //Las contraseñas tienen que ser iguales
+            inputrepPass.setError("Las contraseñas no coinciden");
+            inputrepPass.requestFocus();
+        }else{//To do bien, registramos el usuario
+
+            //Mostramos un progrssDialog de registrando...
+            progressDialog.setTitle("Registrando...");
+            progressDialog.setMessage("Porfavor espera mientras se registra");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+                mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            //Se ha registrado correctamente
+                            progressDialog.dismiss(); //Cancelamos el dialogo de progreso
+                            abrirCrearPerfil(); //Vamos a la ventana de crear el perfil
+                        }else{
+                            progressDialog.dismiss();//Cancelamos el dialogo de progreso
+                            //Mostrar alerta de q no se ha podido registrar
+                            Toast.makeText(CrearCuentaActivity.this,"No se ha podido registrar",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/nerechat/IniciarSesionActivity.java
@@ -0,0 +1,142 @@
+package com.example.nerechat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.nerechat.base.BaseActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class IniciarSesionActivity extends AppCompatActivity {
+    //Pagina de inicio de sesion
+
+    TextInputLayout inputemail, inputpass;
+
+    FirebaseAuth mAuth;
+    ProgressDialog progressDialog;
+    FirebaseUser mUser;
+    DatabaseReference mDatabaseRef;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_sesion);
+
+        //Cargamos las referencias
+        inputemail=findViewById(R.id.inputLEmail);
+        inputpass=findViewById(R.id.inputLPass);
+
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser(); //El usuario actual que tiene iniciada la sesion
+        mDatabaseRef= FirebaseDatabase.getInstance().getReference().child("Perfil"); //para comprobar el perfil del usuario correspondiente
+        progressDialog=new ProgressDialog(this);
+
+        comprobarSiYaHaySesion(); //Con firebase auth, podemos comprobar si esta la sesion iniciada, para asi directamente llevarle a la ventana principal sin tener que iniciar sesion cada vez que cierra y abre la app
+
+        TextView text_crearCuenta= findViewById(R.id.text_crearCuenta); //Crear cuenta
+        text_crearCuenta.setOnClickListener(new View.OnClickListener() {  //Cuando clickemos en el boton camara. pediremos permisos para abrir la camara y guardar las fotos
+            @Override
+            public void onClick(View v) {
+                abrirCrearCuenta();
+            }
+        });
+
+    }
+
+    public void abrirCrearCuenta(){ //Abrimos la pantalla de crear cuenta
+        Intent i = new Intent(this, CrearCuentaActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(i);
+        finish();
+    }
+
+
+    public void abrirPrincipal(){ //Abrimos la pantalla principal porque ya se ha autenticado correctamente
+        Intent i = new Intent(this, ChatUsuariosActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(i);
+        finish();
+    }
+
+    public void abrirCrearPerfil(){ //Abrimos la ventana de que falta crear el perfil de usuario
+        Intent i = new Intent(this, CrearPerfilActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(i);
+        finish();
+    }
+
+    public void iniciarSesion(View v){ //Click en iniciar sesion
+        //Cogemos los correspondientes valores del input. el email y la pass
+        String email=inputemail.getEditText().getText().toString();
+        String pass=inputpass.getEditText().getText().toString();
+
+        if (email.equals("")||!email.contains("@")) { //Pasa que el email sea valido tiene q ser !="" y contener un @. Si no es asi saltamos un error
+            inputemail.setError("El email no es correcto"); //Los tipo input nos permiten mostrar el error directamente, lo que viene muy bien para los inicio de sesion
+            inputemail.requestFocus();
+        }else if(pass.equals("")||pass.length()<6){ //La contraseña tiene que tener un minimo de 6 caracteres (Es una restriccion de las pass de firebase). Si no se cumple mostramos mensaje de error
+            inputpass.setError("La contraseña tiene que tener un minimo de 5 caracteres");
+            inputpass.requestFocus();
+        }else{//Los datos son correctos asique procedemos a iniciar sesion
+            //Mostramos el dialog bar. Que se mantendrá activo hasta que se complete el inicioi de sesion
+            progressDialog.setTitle("Iniciando sesion...");
+            progressDialog.setMessage("Porfavor espera");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        //Se ha registrado correctamente
+                        progressDialog.dismiss(); //Cancelamos la barra de proceso
+                        abrirPrincipal();
+                    }else{
+                        progressDialog.dismiss();
+                        //Mostrar alerta de q no se ha podido registrar
+                        Toast.makeText(IniciarSesionActivity.this,"No se ha podido iniciar sesion. Revisa los datos",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
+
+    public void comprobarSiYaHaySesion(){ //Con firebase, podemos ver si tenemos algun usuario con la sesion iniciada y asi enviarle directamente a la pantalla principal sin que tenga que iniciar sesion
+        if (mUser!=null){  //Si existe un mUser significa que ya tenemos una sesion iniciada
+            Log.d("Logs", "mUser: "+mUser.getUid());
+            //Pero podemos haber abandonado la app sin llegar a crear el perfil del todo.
+            //Comprobamos si tenemos algun perfil creado en la base de datos, de ser asi vamos a la pantalla principal, y en el caso de no tener perfil, llevamos a la pantalla de crear un perfil
+            mDatabaseRef.child(mUser.getUid()).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.exists()){//Comporbamos que ese mUser, tenga creado un perfil. si ya tiene el perfil creado vamos directamente a mainActivicity
+                        abrirPrincipal();
+                    }else{//Si no tiene el usuario creado, es porque ha abandonado en la pagina NuevoUsuarioActivity, asique le llevamos a esa pag para que termine de crear todo bien
+                        abrirCrearPerfil();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) { }
+            });
+        }
+
+    }
+}
